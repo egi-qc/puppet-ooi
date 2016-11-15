939fd3b15d8d5a1ab8343dc3ff657ca3abd1d780
Pablo Orviz <orviz@ifca.unican.es>
2016-11-15 15:18:36 +0000

    boolean to manage repos && get openstack_version through hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,20 @@
-class ooi ($openstack_version,
+class ooi ($openstack_version = hiera("ooi::openstack_version", undef),
            $ooi_listen = "0.0.0.0",
            $ooi_listen_port = 8787,
            $ooi_workers = undef,
+           $manage_repos = true,
            $manage_service = true) {
 
     if ! ($openstack_version in ["liberty", "mitaka"]) {
         fail("Unsupported OpenStack version, choose between 'liberty' or 'mitaka'.")
     }
 
-    class { "ooi::repos": }
+    if $manage_repos {
+        class { "ooi::repos": }
+        $requires = Class["ooi::repos"]
+    }
 
-    class { "ooi::install": require => Class["ooi::repos"]}
+    class { "ooi::install": require => $requires }
 
     class { "ooi::config": require => Class["ooi::install"]}
 
